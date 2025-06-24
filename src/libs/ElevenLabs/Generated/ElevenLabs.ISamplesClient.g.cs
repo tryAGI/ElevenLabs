9a7631d644a44f9ec37c6384c0637b58bcbcecfe
Konstantin S. <havendv@gmail.com>
2025-06-24 05:44:57 +0400

    feat: Updated OpenAPI spec (#125)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.ISamplesClient.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.ISamplesClient.g.cs
@@ -4,7 +4,6 @@
 namespace ElevenLabs
 {
     /// <summary>
-    /// Access to your samples. A sample is any audio file you attached to a voice. A voice can have one or more samples.<br/>
     /// If no httpClient is provided, a new one will be created.<br/>
     /// If no baseUri is provided, the default baseUri from OpenAPI spec will be used.
     /// </summary>
