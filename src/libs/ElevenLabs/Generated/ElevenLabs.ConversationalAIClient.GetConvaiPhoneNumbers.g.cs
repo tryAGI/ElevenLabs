85ae5fdda94ba17d3c83be15e3d117ccbaba5384
Konstantin S. <havendv@gmail.com>
2025-06-27 16:41:36 +0400

    feat: Updated OpenAPI spec (#128)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.ConversationalAIClient.GetConvaiPhoneNumbers.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.ConversationalAIClient.GetConvaiPhoneNumbers.g.cs
@@ -41,7 +41,7 @@ namespace ElevenLabs
                 xiApiKey: ref xiApiKey);
 
             var __pathBuilder = new global::ElevenLabs.PathBuilder(
-                path: "/v1/convai/phone-numbers/",
+                path: "/v1/convai/phone-numbers",
                 baseUri: HttpClient.BaseAddress); 
             var __path = __pathBuilder.ToString();
             using var __httpRequest = new global::System.Net.Http.HttpRequestMessage(
