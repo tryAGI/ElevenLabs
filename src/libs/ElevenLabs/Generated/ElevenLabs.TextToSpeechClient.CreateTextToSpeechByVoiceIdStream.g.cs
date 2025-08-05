901ad6c1d83e19adb5f180b15d298b7ffea4baac
Konstantin S. <havendv@gmail.com>
2025-08-05 19:26:31 +0400

    feat: Updated OpenAPI spec (#152)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.TextToSpeechClient.CreateTextToSpeechByVoiceIdStream.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.TextToSpeechClient.CreateTextToSpeechByVoiceIdStream.g.cs
@@ -321,7 +321,7 @@ namespace ElevenLabs
         /// Example: [3tPgBrD1UdW3snUkGw1K, 4D1jAxiRFkolBNUGzXkU, 4c8Z4aWliVR2oipYRXhj]
         /// </param>
         /// <param name="applyTextNormalization">
-        /// This parameter controls text normalization with three modes: 'auto', 'on', and 'off'. When set to 'auto', the system will automatically decide whether to apply text normalization (e.g., spelling out numbers). With 'on', text normalization will always be applied, while with 'off', it will be skipped. Cannot be turned on for 'eleven_turbo_v2_5' or 'eleven_flash_v2_5' models.<br/>
+        /// This parameter controls text normalization with three modes: 'auto', 'on', and 'off'. When set to 'auto', the system will automatically decide whether to apply text normalization (e.g., spelling out numbers). With 'on', text normalization will always be applied, while with 'off', it will be skipped. For 'eleven_turbo_v2_5' and 'eleven_flash_v2_5' models, text normalization can only be enabled with Enterprise plans.<br/>
         /// Default Value: auto<br/>
         /// Example: true
         /// </param>
