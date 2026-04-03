46bb31317ef801b7dd92cf60e7b7da317792a4b5
Konstantin S. <havendv@gmail.com>
2026-04-03 09:03:54 +0400

    feat: Updated OpenAPI spec (#273)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.MusicClient.Compose.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.MusicClient.Compose.g.cs
@@ -214,7 +214,6 @@ namespace ElevenLabs
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsByteArrayAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
