d69b4f4136c2c02473f4f5646a0ec2cda4bc2349
Konstantin S. <havendv@gmail.com>
2026-05-06 06:39:30 +0400

    feat: Updated OpenAPI spec (#344)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.IUserClient.Get.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.IUserClient.Get.g.cs
@@ -2,26 +2,26 @@
 
 namespace ElevenLabs
 {
-    public partial interface IElevenLabsClient
+    public partial interface IUserClient
     {
         /// <summary>
-        /// Get User Subscription Info<br/>
-        /// Gets extended information about the users subscription
+        /// Get User Info<br/>
+        /// Gets information about the user
         /// </summary>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::ElevenLabs.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::ElevenLabs.ExtendedSubscriptionResponseModel> GetAsync(
+        global::System.Threading.Tasks.Task<global::ElevenLabs.UserResponseModel> GetAsync(
             global::ElevenLabs.AutoSDKRequestOptions? requestOptions = default,
             global::System.Threading.CancellationToken cancellationToken = default);
         /// <summary>
-        /// Get User Subscription Info<br/>
-        /// Gets extended information about the users subscription
+        /// Get User Info<br/>
+        /// Gets information about the user
         /// </summary>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::ElevenLabs.ApiException"></exception>
-        global::System.Threading.Tasks.Task<global::ElevenLabs.AutoSDKHttpResponse<global::ElevenLabs.ExtendedSubscriptionResponseModel>> GetAsResponseAsync(
+        global::System.Threading.Tasks.Task<global::ElevenLabs.AutoSDKHttpResponse<global::ElevenLabs.UserResponseModel>> GetAsResponseAsync(
             global::ElevenLabs.AutoSDKRequestOptions? requestOptions = default,
             global::System.Threading.CancellationToken cancellationToken = default);
     }
