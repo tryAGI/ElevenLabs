a5a4f9d8b37ba18acecf1fb8d2e098e1b91cb05f
Konstantin S. <havendv@gmail.com>
2026-08-27 04:26:36 +0400

    feat: Updated OpenAPI spec (#566)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ElevenLabs/Generated/ElevenLabs.Models.ConversationConfigInput.g.cs
+++ src/libs/ElevenLabs/Generated/ElevenLabs.Models.ConversationConfigInput.g.cs
@@ -48,6 +48,12 @@ namespace ElevenLabs
         public global::System.Collections.Generic.IList<global::ElevenLabs.ClientEvent>? MonitoringEvents { get; set; }
 
         /// <summary>
+        /// Configure DTMF (keypad) input collection during phone calls
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("dtmf_input_settings")]
+        public global::ElevenLabs.DTMFInputConfig? DtmfInputSettings { get; set; }
+
+        /// <summary>
         /// Configuration for background sound during conversations.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("background_sound")]
@@ -90,6 +96,9 @@ namespace ElevenLabs
         /// <param name="monitoringEvents">
         /// The events that will be sent to monitoring connections.
         /// </param>
+        /// <param name="dtmfInputSettings">
+        /// Configure DTMF (keypad) input collection during phone calls
+        /// </param>
         /// <param name="backgroundSound">
         /// Configuration for background sound during conversations.
         /// </param>
@@ -107,6 +116,7 @@ namespace ElevenLabs
             global::ElevenLabs.FileInputConfig? fileInput,
             bool? monitoringEnabled,
             global::System.Collections.Generic.IList<global::ElevenLabs.ClientEvent>? monitoringEvents,
+            global::ElevenLabs.DTMFInputConfig? dtmfInputSettings,
             global::ElevenLabs.BackgroundSoundConfig? backgroundSound,
             bool? sourceAttribution)
         {
@@ -116,6 +126,7 @@ namespace ElevenLabs
             this.FileInput = fileInput;
             this.MonitoringEnabled = monitoringEnabled;
             this.MonitoringEvents = monitoringEvents;
+            this.DtmfInputSettings = dtmfInputSettings;
             this.BackgroundSound = backgroundSound;
             this.SourceAttribution = sourceAttribution;
         }
